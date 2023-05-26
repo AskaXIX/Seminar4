66b86ef0ae8c30bfe14b222b41f18469d96c7265
Aslan <aslan.aslan.14@bk.ru>
2023-05-27 03:12:03 +0600

    all tasks done

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+int ExNumbers(int A, int B)
+{
+    int res = A;
+    int count = 1;
+    
+    while (count < B)
+    {
+        res = res * A;
+        count++;
+    }
+    return res;
+}
+
+int first_number = ReadInt("Insert first number: ");
+int second_number = ReadInt("Insert second number: ");
+int res = ExNumbers(first_number, second_number);
+System.Console.WriteLine(res);
---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] FillArray(int size, int left_range, int right_range)
+{
+    int[] temp = new int[size];
+    Random rand = new Random();
+
+    for(int i = 0; i < size; i++)
+    {
+        temp[i] = rand.Next(left_range, right_range + 1);
+    }
+    return temp;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+int[] array = FillArray(8, 0, 10);
+
+PrintArray(array);
